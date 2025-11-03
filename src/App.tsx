43d1daa587a13da63257821a6931fa926d78f12b
PlacideLinkpehoun <placidelinkpehoun.com>
2025-11-03 15:38:57 +0100

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { Toaster } from "./app/components/ui/toaster";
-import { Toaster as Sonner } from "./app/components/ui/sonner";
 import { TooltipProvider } from "./app/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -10,8 +8,6 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
