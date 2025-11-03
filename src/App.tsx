8dda63a1c31658a3acbca0cdb60acc46bc4c03ac
PlacideLinkpehoun <placidelinkpehoun.com>
2025-11-03 15:11:37 +0100

    notfound_correction

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "./app/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./app/page";
-import NotFound from "./app/pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -16,8 +15,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
